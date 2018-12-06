64113ffc78cc1a5e1bec55e8194f0f2583f6de6b
bambooJin <993234913@qq.com>
2018-12-06 14:01:36 +0800

    # Jin: head commit changed in the meantime
    新增图片上传和图片显示接口

--- src/main/java/com/group/shop/config/webconfig/MyWebAppConfigurer.java
+++ src/main/java/com/group/shop/config/webconfig/MyWebAppConfigurer.java
@@ -3,7 +3,13 @@ package com.group.shop.config.webconfig;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.MultipartConfigElement;
+
 import com.group.shop.config.interceptor.LoginInterceptor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -43,6 +49,23 @@ public class MyWebAppConfigurer implements WebMvcConfigurer{
         /** 不拦截部分 **/
         loginIR.excludePathPatterns("/admin/login");
     }
+    
+    /**
+     * 在配置文件中配置的文件保存路径
+     */
+    @Value("${img.location}")
+    private String location;
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大KB,MB
+        factory.setMaxFileSize("2MB");
+        //设置总上传数据总大小
+        factory.setMaxRequestSize("10MB");
+        return factory.createMultipartConfig();
+    }
+
 
 }
 
